a184d26e7bb06842fcd0657d937e85144bdd147d
Subhadipta Nayak <nsubhadipta@gmail.com>
2020-09-26 15:14:38 +0530

    Update database.php

--- config/database.php
+++ config/database.php
@@ -45,7 +45,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
+            'url' =>'https://db4free.net/phpMyAdmin/',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => 'XXXX',
             'database' => 'ecommerce421',
